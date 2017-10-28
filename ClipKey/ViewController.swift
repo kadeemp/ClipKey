53cfda5d59fcdd3bce4fbd9a04b07ce02eac78ba
Kadeem Palacios <kadeempalacios@gmail.com>
2017-10-27 18:39:20 -0700

    Initial Commit

---
+++ ClipKey/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ClipKey
+//
+//  Created by Kadeem Palacios on 10/27/17.
+//  Copyright © 2017 Kadeem Palacios. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
