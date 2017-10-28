4d6131d8228bf59096452e9d20b26b9c5307f3c0
Kadeem Palacios <kadeempalacios@gmail.com>
2017-10-27 22:18:59 -0700

    Added basic compenents to initial view controller

--- ClipKey/ViewController.swift
+++ ClipKey/ViewController.swift
@@ -8,18 +8,36 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.title = "ClipKey"
+        navigationItem.rightBarButtonItem = editButtonItem
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    var titles = ["First", "Last", "Full Name", "Address", "City", "Zip Code"]
+    var keys = ["Kadeem", "Palacios", "Kadeem Palacios", "2443 Fitzpatrick st.", "San Pablo, Ca", "94806"]
+
+    @IBOutlet weak var keysTable: UITableView!
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return titles.count
     }
 
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = keysTable.dequeueReusableCell(withIdentifier: "cell")
+        cell?.textLabel?.text = titles[indexPath.row]
+        return cell!
+    }
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
+
+
+        if editingStyle == .delete {
+            titles.remove(at: indexPath.row)
+            keysTable.deleteRows(at: [indexPath] , with: .fade)
+        }
+    }
 
 }
 
